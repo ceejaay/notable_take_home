f832af319f389eea3b334ab4d7f1737a657916e9
Chad Jemmett <cjemmett@wehco.com>
2022-04-03 12:49:15 -0500

    removed shiren

--- shiren/serializers.py
+++
@@ -1,9 +0,0 @@
-from shiren.models import Swords
-from rest_framework import serializers
-
-
-class SwordSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Swords
-        fields = ['name', 'type']
-
