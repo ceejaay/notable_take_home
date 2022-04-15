22b60fb26f74ba023ae48e1fa13d80f594ec4d40
Chad Jemmett <cjemmett@wehco.com>
2022-04-14 22:29:32 -0500

    Removed the .pyc files

--- shiren/models.py
+++ shiren/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Swords(models.Model):
     name = models.CharField(max_length=256)
     type = models.CharField(max_length=256)
+
+class Prices(models.Model):
+    price = models.IntegerField()
+
+class Scrolls(models.Model):
+    name = model.CharField()
+    price_id = models.ForeignKey('prices', on_delete.CASCADE)
+
