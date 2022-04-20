a1df9505c3e96ca823382fbb055273437850868b
Chad Jemmett <cjemmett@wehco.com>
2022-04-20 17:59:40 -0500

    added csrf settings

--- notable/settings.py
+++ notable/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "127.0.0.1, localhost:8000").split(",")
 ALLOWED_HOSTS = ['localhost', ".herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = [".herokuapp.com"]
 
 APPEND_SLASH=False
 
@@ -153,4 +154,3 @@ REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 10
         }
-CSRF_TRUSTED_ORIGINS=['.herokuapp.com']
